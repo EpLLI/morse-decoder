33c089a14b5a6d5b15aa82b93386b0eb5ae4cf79
EpLLI <staniush.iv@gmail.com>
2020-10-25 16:39:29 +0300

    feat: add functional

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let z=expr.length/10;
+    let b='';
+    let t='';
+    let c=0;
+    let p='';
+    let phr=[];
+    for(let i=z;i>0;i-- ){  
+        p='';
+        b=expr.substring(c*10,++c*10);
+        for(let j=0;j<5;j++){
+            t=b.substring(j*2,++j*2);
+            j--;
+            if(t==='00'){
+                p+='';
+            } else if(t==='10'){
+                p+='.';
+            }else if(t==='11'){
+                p+='-';
+            }else if(t==='**'){
+                p+=' ';
+            }
+
+         }
+         if (p==='     '){
+            phr.push(' ');
+         }
+         else {
+         phr.push(MORSE_TABLE[p]);
+         }
+        
+    }
+    return phr.join("");
 }
 
 module.exports = {
